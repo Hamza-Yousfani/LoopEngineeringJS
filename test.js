930448ec36d04bc8a6dfb2fc52f873a514c3d6d2
levant <168703677+Hamza-Yousfani@users.noreply.github.com>
2026-07-16 16:22:20 +0500

    Create test.js

---
+++ test.js
@@ -0,0 +1,30 @@
+function getAdults(users) {
+    var adults = [];
+
+    for (var i = 0; i < users.length; i++) {
+        if (users[i].age >= 18) {
+            adults.push(users[i].name);
+        }
+    }
+
+    return adults;
+}
+
+var users = [
+    {
+        name: "John",
+        age: 25
+    },
+    {
+        name: "Alice",
+        age: 16
+    },
+    {
+        name: "Bob",
+        age: 30
+    }
+];
+
+var result = getAdults(users);
+
+console.log(result);
