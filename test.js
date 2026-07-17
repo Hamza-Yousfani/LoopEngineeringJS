e88796fa392ff3987f42e78c5ad6db2570b118af
levant <168703677+Hamza-Yousfani@users.noreply.github.com>
2026-07-17 15:12:12 +0500

    fixed to match ES6+ standards

--- test.js
+++ test.js
@@ -1,33 +1,14 @@
-function getAdults(users) {
-    var adults = [];
-     //broken 
+const getAdults = (users) => users
+    .filter(user => user.age >= 18)
+    .map(user => user.name);
 
-    for (var i = 0; i < users.length; i++) {
-        if (users[i].age >= 18) {
-            adults.push(users[i].name);
-        }
-    }
-
-    return adults;
-}
-
-var users = [
-    {
-        name: "John",
-        age: 25
-    },
-    {
-        name: "Alice",
-        age: 16
-    },
-    {
-        name: "Bob",
-        age: 30
-    }
+const users = [
+    { name: "John", age: 25 },
+    { name: "Alice", age: 16 },
+    { name: "Bob", age: 30 }
 ];
 
-var result = getAdults(users);
-
+const result = getAdults(users);
 
 console.log(result);
 console.log("Such a frustrsaating folly");
